787a7f3447e936971b1888e980a3e81dac906550
Alexander Justin King Sing Ortiz <ajkso92@gmail.com>
2022-05-17 17:58:53 -0400

    restructuring for financial simulator

---
+++ simulators.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import yfinance as yf
+import supports
+
+def bonds():
+    pass
+
+def stock():
+    pass
+
+def portfolio():
+    pass
---
+++ supports.py
@@ -0,0 +1,2 @@
+def test(test:str) -> str:
+    print(test)
